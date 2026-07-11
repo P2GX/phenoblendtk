fcad39f18b63909e0e400342f6ad1816abe7ccaa
Peter N. Robinson <peter.robinson@bih-charite.de>
2026-07-11 18:08:50 +0200

    Adding help page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NewCaseComponent } from './newppkt/newppkt.component';
 import { PhenotypeProfileVisualizerComponent } from './visualize/visualize.component';
-import { HpoTwostepComponent } from './util/hpotwostep/hpotwostep.component';
 import { GeneDiseaseComponent } from './util/genedisease/genedisease.component';
+import { HelpComponent } from './help/help.component';
 
 
 export const appRoutes: Routes = [
@@ -12,6 +12,7 @@ export const appRoutes: Routes = [
   { path: 'newppkt', component: NewCaseComponent },
   { path: 'visualize', component: PhenotypeProfileVisualizerComponent },
   { path: 'genedisease', component: GeneDiseaseComponent },
+  { path: 'help', component: HelpComponent },
   
   
   // The wildcard safety net stays at the bottom
